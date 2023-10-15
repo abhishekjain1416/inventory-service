162d59abfe4e95bc417ac98fba9b0de389fd16c1
Abhishek Jain <abhishek.jain@docquity.com>
2023-10-15 17:40:04 +0530

    Refact: Change Path Variable to Request Param

--- src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
+++ src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
@@ -1,13 +1,16 @@
 package com.programmingtechie.inventoryservice.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
 import com.programmingtechie.inventoryservice.service.InventoryService;
 
 @RestController
@@ -17,9 +20,9 @@ public class InventoryController {
     @Autowired
     private InventoryService inventoryService;
     
-    @GetMapping("/{sku-code}")
+    @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
 
         return inventoryService.isInStock(skuCode);
     }
