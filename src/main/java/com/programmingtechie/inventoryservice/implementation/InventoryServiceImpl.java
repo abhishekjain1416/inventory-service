b3054a28515196081f3dc6c1eded043cda2ee024
Abhishek Jain <abhishekjain1416@gmail.com>
2024-01-16 16:45:03 +0530

    Strategy Design Pattern to achieve loose coupling

---
+++ src/main/java/com/programmingtechie/inventoryservice/implementation/InventoryServiceImpl.java
@@ -0,0 +1,44 @@
+package com.programmingtechie.inventoryservice.implementation;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.programmingtechie.inventoryservice.dto.InventoryResponse;
+import com.programmingtechie.inventoryservice.repository.InventoryRepository;
+import com.programmingtechie.inventoryservice.service.InventoryService;
+
+@Component
+public class InventoryServiceImpl implements InventoryService {
+
+	@Autowired
+    private InventoryRepository inventoryRepository;
+
+    /**
+     * This method checks if the list of SKU codes is in stock and returns a list of InventoryResponse objects.
+     * 
+     * This method is used for retrieving data and not for modifying the database.
+     * i.e. the transaction is read-only and does not involve any write operations.
+     * 
+     * @param skuCode
+     * @return
+     */
+	@Override
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+
+        // Retrieve inventory items from the repository based on the provided SKU codes
+        return inventoryRepository.findBySkuCodeIn(skuCode)
+            // Convert the list of Inventory objects to a list of InventoryResponse objects
+            .stream()
+            .map(inventory -> 
+                // Map each Inventory object to an InventoryResponse object
+                InventoryResponse.builder()
+                    .skuCode(inventory.getSkuCode())
+                    .isInStock(inventory.getQuantity() > 0)
+                    .build()
+            ).toList();
+    }
+}
--- src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
+++ src/main/java/com/programmingtechie/inventoryservice/service/InventoryService.java
@@ -10,33 +10,8 @@ import com.programmingtechie.inventoryservice.dto.InventoryResponse;
 import com.programmingtechie.inventoryservice.repository.InventoryRepository;
 
 @Service
-public class InventoryService {
-    
-    @Autowired
-    private InventoryRepository inventoryRepository;
+public interface InventoryService {
 
-    /**
-     * This method checks if the list of SKU codes is in stock and returns a list of InventoryResponse objects.
-     * 
-     * This method is used for retrieving data and not for modifying the database.
-     * i.e. the transaction is read-only and does not involve any write operations.
-     * 
-     * @param skuCode
-     * @return
-     */
-    @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode){
-
-        // Retrieve inventory items from the repository based on the provided SKU codes
-        return inventoryRepository.findBySkuCodeIn(skuCode)
-            // Convert the list of Inventory objects to a list of InventoryResponse objects
-            .stream()
-            .map(inventory -> 
-                // Map each Inventory object to an InventoryResponse object
-                InventoryResponse.builder()
-                    .skuCode(inventory.getSkuCode())
-                    .isInStock(inventory.getQuantity() > 0)
-                    .build()
-            ).toList();
-    }
+    // This method checks if the list of SKU codes is in stock and returns a list of InventoryResponse objects.
+    List<InventoryResponse> isInStock(List<String> skuCode);
 }
